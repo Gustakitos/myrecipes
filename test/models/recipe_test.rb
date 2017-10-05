d624ed96e06b8bb3124b15117a6224f408236e59
Gustakitos <luisgustavolg1033@gmail.com>
2017-10-05 00:11:10 -0300

    Add one to many assossiation between recipe and chef

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name:"vegetable",description:"Greate vegetable recipe")
+    @chef = Chef.create!(chefname: "Gustavo", email: "gustavo@example.com")
+    @recipe = @chef.recipes.build(name: "vegetable", description: "Greate vegetable recipe")
+  end
+
+  test "Recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "Recipe should be valid" do
