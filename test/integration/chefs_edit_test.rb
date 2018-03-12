02ad16d3d98f90fb8ec8c8e800450d8ad9c8d552
Gustakitos <luisgustavolg1033@gmail.com>
2018-03-12 19:24:25 -0300

    Complete authentication system with tests

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -7,6 +7,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "reject an invalid edit" do
+    sign_in_as(@chef,"password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
       patch chef_path(@chef), params: {chef: {chefname: " ", email: "gustavo@example.com"}}
@@ -16,6 +17,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "accept valid signup" do
+    sign_in_as(@chef,"password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: {chef: {chefname: "gustavo1", email: "gustavo1@example.com"}}
